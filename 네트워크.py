5eb6339339c7e55ad74e8d785147236162595a1d
dldbdud314 <dldbdud314@naver.com>
2022-04-06 19:18:03 +0900

    [fix]: 네트워크 - 양방향 그래프로 수정+dfs

--- 네트워크.py
+++ 네트워크.py
@@ -1,11 +1,16 @@
-from collections import deque
+def dfs(x, graph, visited):
+    if visited[x] == True:
+        return
+    visited[x] = True
+    for val in graph[x]:
+        dfs(val, graph, visited)
 
 def make_graph(n, matrix):
     graph = dict()
     for i in range(n):
         graph[i] = []
-        for j in range(i+1, n):
-            if matrix[i][j] == 1:
+        for j in range(n):
+            if i != j and matrix[i][j] == 1:
                 graph[i].append(j)
     return graph
 
@@ -13,18 +18,8 @@ def solution(n, computers):
     graph = make_graph(n, computers)
     cnt = 0
     visited = [False] * n
-    for key in graph.keys():
-        if visited[key] == False:
-            queue = deque([key])
-            while queue:
-                cur = queue.popleft()
-                if visited[cur] == False:
-                    visited[cur] = True
-                    for v in graph[cur]:
-                        queue.append(v)
+    for x in graph:
+        if visited[x] == False:
+            dfs(x, graph, visited)
             cnt += 1
     return cnt
-
-solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
-
-#뭐지 무ㅓ가 잘못된걸까..
