5bb25bd82673ba5a072a438990f92a3b5f57dac0
dldbdud314 <dldbdud314@naver.com>
2022-04-20 20:04:24 +0900

    [update]: 1062

--- 백준/1062.py
+++ 백준/1062.py
@@ -1,7 +1,7 @@
 #가르침
 from collections import defaultdict
 
-def get_max_count(n, k, word_list):
+def get_max_count1(k, word_list):
     if k < 5: return 0
     
     dictionary = defaultdict(int)
@@ -27,9 +27,32 @@ def get_max_count(n, k, word_list):
     
     return cnt
 
+from itertools import combinations
+#시간초과ㅠㅠ
+def get_max_count2(k, word_list):
+    if k < 5: return 0
+    
+    default_set = {'a', 'n', 't', 'i', 'c'}
+    cropped_word_list = [word[4:-4] for word in word_list]
+    word_set_list = [set(x) for x in cropped_word_list]
+    word_set_list = [word_set - default_set for word_set in word_set_list]
+    total_char_set = set()
+    for word_set in word_set_list:
+        total_char_set.update(word_set)
+    total_char_list = list(total_char_set)
+    m = k - 5
+    if len(total_char_list) < k-5: m = len(total_char_list)
+    max_cnt = -float('inf')
+    for char_set in combinations(total_char_list, m):
+        cnt = 0
+        for word_set in word_set_list:
+            if word_set < set(char_set) or word_set == set(char_set): cnt += 1
+        max_cnt = max(max_cnt, cnt)
+    return max_cnt
+    
 n, k = map(int, input().split())
 word_list = [input() for _ in range(n)]
 
-print(get_max_count(n, k, word_list))
+print(get_max_count2(k, word_list))
 
 #흠... 맞왜틀.....
