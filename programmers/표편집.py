7af650b98910aa7a9d4bc6e951e1bd46d215af0c
dldbdud314 <dldbdud314@naver.com>
2022-05-03 11:23:19 +0900

    [fix]: 표편집 - 정확성, 효율성 개선

--- programmers/표편집.py
+++ programmers/표편집.py
@@ -9,34 +9,39 @@ def initialize_linked_list(n):
 def solution(n, k, cmd):
     linked_list = initialize_linked_list(n)
     cp = k
-    total_set = {i for i in range(n)}
+    answer = ['O'] * n
     del_stack = []
     for x in cmd:
-        if x[0] == 'U':
-            move = int(x[2])
-            cur_list = list(total_set - set(del_stack))
-            cp = cur_list[cur_list.index(cp) - move]
-        elif x[0] == 'D':
-            move = int(x[2])
-            cur_list = list(total_set - set(del_stack))
-            cp = cur_list[cur_list.index(cp) + move]
-        elif x[0] == 'C':
-            del_stack.append(cp)
-            left_idx = linked_list[cp]['lp']
-            right_idx = linked_list[cp]['rp']
-            if right_idx == -1: #cp == last node
-                linked_list[left_idx]['rp'] = -1
-                cp = left_idx
-            else:    
-                linked_list[left_idx]['rp'] = right_idx
-                linked_list[right_idx]['lp'] = left_idx
-                cp = right_idx       
-        elif x[0] == 'Z':
-            new_idx = del_stack.pop()
-            left_idx = linked_list[new_idx]['lp']
-            right_idx = linked_list[new_idx]['rp']
-            linked_list[left_idx]['rp'] = new_idx
-            linked_list[right_idx]['lp'] = new_idx
-    return ''.join(['X' if i in set(del_stack) else 'O' for i in range(n)])
+        if len(x) > 1:
+            d, move = x.split()
+            if d == 'U':
+                for _ in range(int(move)): 
+                    cp = linked_list[cp]['lp']
+            elif d == 'D':
+                for _ in range(int(move)): 
+                    cp = linked_list[cp]['rp']
+        else:
+            if x == 'C':
+                answer[cp] = 'X'
+                del_stack.append(cp)
+                left_idx = linked_list[cp]['lp']
+                right_idx = linked_list[cp]['rp']
+                if right_idx == -1: #cp == last node
+                    linked_list[left_idx]['rp'] = -1
+                    cp = left_idx
+                else:    
+                    linked_list[left_idx]['rp'] = right_idx
+                    linked_list[right_idx]['lp'] = left_idx
+                    cp = right_idx       
+            else:
+                new_idx = del_stack.pop()
+                answer[new_idx] = 'O'
+                left_idx = linked_list[new_idx]['lp']
+                right_idx = linked_list[new_idx]['rp']
+                linked_list[left_idx]['rp'] = new_idx
+                linked_list[right_idx]['lp'] = new_idx
+    return ''.join(answer)
     
 print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
+
+#ㅌㅔ스트케이스 하나 뭘까....ㅂㄷ
