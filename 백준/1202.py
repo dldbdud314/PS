2b393063f3505740204ed81a22e2b966460a8e9b
dldbdud314 <dldbdud314@naver.com>
2022-06-11 11:17:33 +0900

    [update] :1202

--- 백준/1202.py
+++ 백준/1202.py
@@ -1,26 +1,23 @@
 #보석도둑
-#시간초과 
-from collections import deque
-from heapq import heappush, heappop
+#시간초과.. ha...
+from heapq import heappush, heappop, heapify
 
 def solution(gems, bags):
-    gems.sort()
-    gem_queue = deque(gems)
+    heapify(gems)
     bags.sort()
-    total = 0
-    heap = []
-    #이중루프X - 어떻게 개선할지 모르겠다
-    for bag in bags:
-        while gem_queue and gem_queue[0][0] <= bag:
-            _, price = gem_queue.popleft()
-            heappush(heap, (-price, price))
-        if not heap: continue
-        total += heappop(heap)[1]
-        
-    return total
+    w = 0
+    for c in bags:
+        tmp_heap = [] #무게를 담는 최대힙
+        while gems and gems[0][0] <= c:
+            m, v = heappop(gems)
+            heappush(tmp_heap, (-v, v, m))
+        w += heappop(tmp_heap)[1]
+        while tmp_heap: #나머지는 다시 담기
+            _, v, m = heappop(tmp_heap)
+            heappush(gems, [m, v])
+    return w
         
 n, k = map(int, input().split())
 gems = [list(map(int, input().split())) for _ in range(n)]
 bags = [int(input()) for _ in range(k)]
-
 print(solution(gems, bags))
