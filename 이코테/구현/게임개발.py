740dae03aeb9f5deb47342b898af6ad87b941536
dldbdud314 <dldbdud314@naver.com>
2022-07-14 16:29:40 +0900

    [fix]:게임개발

--- 이코테/구현/게임개발.py
+++ 이코테/구현/게임개발.py
@@ -8,32 +8,30 @@ def move_forward(d): #해당 방향으로 움직이기
 def move_backward(d): #반대 방향으로 움직이기
     return {0 : (1, 0), 1 : (0, -1), 2 : (-1, 0), 3 : (0, 1)}.get(d)
 
-
 def solution(game_map, n, m, y, x, d):
     visited = [[False] * m for _ in range(n)]
-    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    visited[y][x] = True
+    turn_time = 0
     res = 1
     while True:
-        #현재 위치에서 네 방향으로 갈수 있는지부터 check
-        flag = False
-        for dy, dx in dirs:
-            if game_map[y+dy][x+dx] == 0 and not visited[y+dy][x+dx]:
-                flag = True #하나라도 갈 수 있으면 True
-                break
-        if flag:
-            d = turn_left(d)
-            dy, dx = move_forward(d)
-            if game_map[y+dy][x+dx] == 0 and not visited[y+dy][x+dx]:
-                y += dy
-                x += dx
-                visited[y+dy][x+dx] = True
-                res += 1
-        else:
+        d = turn_left(d)
+        dy, dx = move_forward(d) #좌표만 얻는 거
+        if game_map[y+dy][x+dx] == 0 and not visited[y+dy][x+dx]:
+            y += dy
+            x += dx
+            visited[y][x] = True
+            res += 1
+            turn_time = 0
+            continue
+        else: 
+            turn_time += 1
+        if turn_time == 4:
             dy, dx = move_backward(d)
             if game_map[y+dy][x+dx] == 1:
                 break
             y += dy
             x += dx
+            turn_time = 0
     return res
 
 n, m = map(int, input().split())
