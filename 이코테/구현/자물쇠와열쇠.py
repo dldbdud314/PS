db11a1faa2022b27151a45047cc62c7450827b07
dldbdud314 <dldbdud314@naver.com>
2022-07-17 13:26:42 +0900

    [fix]: 자물쇠와 열쇠

--- 이코테/구현/자물쇠와열쇠.py
+++ 이코테/구현/자물쇠와열쇠.py
@@ -12,31 +12,27 @@ def turn(key):
     return new_key
 
 def is_open(a, m, n):
-    print(a)
     for i in range(m, m + n):
         for j in range(m, m + n):
             if a[i][j] == 0 or a[i][j] == 2: #홈이 안 채워진 경우 or 돌기와 돌기가 만나는 경우
                 return False
-    print("true")
     return True
 
 def solution(key, lock):
     m, n = len(key), len(lock)
     a = init_a(m, n, lock) #공간 초기화, 중간에 자물쇠 박제
     #열쇠 4번 회전 -> 이동하면서 겹치는 부분 확인
-    for i in range(4):
-        for k in range(1, m + n):
-            for ki, li in zip(range(m), range(k, k + m)):
-                for kj, lj in zip(range(m), range(k, k + m)):
-                    if a[li][lj] == 1:
-                        a[li][lj] += key[ki][kj]
-                    else:
-                        a[li][lj] = key[ki][kj]
-            if is_open(a, m, n): #자물쇠를 열 수 있다면
-                return True 
-            a = init_a(m, n, lock) #공간 초기화
-        #90도 회전
-        key = turn(key)
+    for _ in range(4):
+        for ki in range(n - m + 1, n + m):
+            for kj in range(n - m + 1, n + m):
+                for i in range(m):
+                    for j in range(m):
+                        a[ki + i][kj + j] += key[i][j]
+                if is_open(a, m, n): #자물쇠를 열 수 있다면
+                    return True 
+                a = init_a(m, n, lock) #공간 초기화
+            #90도 회전
+            key = turn(key)
     return False
-print(solution([[0, 0], [0, 0]], [[1, 0, 0], [1, 0, 0], [1, 1, 1]]))
-# 42.0/100.0
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
+# 89.0/100.0
