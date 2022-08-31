d2123bef57c01c0d4fc90c7586c3f7c3f4b59976
dldbdud314 <dldbdud314@naver.com>
2022-08-31 13:08:39 +0900

    dp

--- 이코테/다이나믹프로그래밍/효율적인화폐구성.py
+++ 이코테/다이나믹프로그래밍/효율적인화폐구성.py
@@ -1,16 +1,11 @@
-#풀참
 def solution(n, coins, m):
     dp = [10001] * (m + 1)
     dp[0] = 0
     for i in range(n):
-        for k in range(coins[i], m + 1): 
-            dp[k] = min(dp[k], dp[k - coins[i]] + 1)
-        print(dp)
-    if dp[m] == 10001:
-        return -1
-    else:
-        return dp[m]
-    
+        for j in range(coins[i], m + 1):
+            dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+    return -1 if dp[m] == 10001 else dp[m]
+
 n, m = map(int, input().split())
 coins = []
 for _ in range(n):
