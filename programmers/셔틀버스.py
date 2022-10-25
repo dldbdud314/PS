d0d86610cb7d9c6d97c45a91946b31490a4cf621
dldbdud314 <dldbdud314@naver.com>
2022-10-25 20:19:49 +0900

    [fix] : 셔틀버스

--- programmers/셔틀버스.py
+++ programmers/셔틀버스.py
@@ -1,44 +1,45 @@
-# 합계 : 62.5 / 100.0 - to fix ..
 def solution(n, t, m, timetable):
-    before_last = "00:01"
-    last_bus = "09:00"
-    if n > 1:
-        for _ in range(n - 1):
-            before_last = last_bus
-            hour, min = map(int, last_bus.split(':'))
-            if min + t >= 60:
-                min = min + t - 60
-                hour = hour + 1
-            else:
-                min += t
-            str_hour = '0' + str(hour) if hour < 10 else str(hour)
-            str_min = '0' + str(min) if min < 10 else str(min)
-            last_bus = str_hour + ':' + str_min
-    start_hour, start_min = map(int, before_last.split(':'))
-    end_hour, end_min = map(int, last_bus.split(':'))
-    start_time = start_hour * 60 + start_min if start_hour > 0 else start_min
-    end_time = end_hour * 60 + end_min if end_hour > 0 else end_min
-    crew = []
-    for time in timetable:
-        hour, min = map(int, time.split(':'))
-        crew_time = hour * 60 + min if hour > 0 else min
-        if start_time <= crew_time <= end_time:
-            crew.append(time)
-    crew.sort()
-    ans = ''
-    if len(crew) >= m:
-        last_crew = crew[-1]
-        hour, min = map(int, last_crew.split(':'))
-        if min == 0:
-            hour -= 1
-            min = 59
-        else:
-            min -= 1
-        str_hour = '0' + str(hour) if hour < 10 else str(hour)
-        str_min = '0' + str(min) if min < 10 else str(min)
-        ans = str_hour + ':' + str_min
-    else:
-        ans = last_bus
-    return ans
+    buses = [9 * 60]
+    time = 9 * 60  # 시작 버스는 무조건 9시
+    for _ in range(1, n):
+        time += t
+        buses.append(time)
 
-print(solution(10, 25, 1, ["09:00", "09:10", "09:20", "09:30", "09:40" ,"09:50", "10:00", "10:10", "10:20", "10:30", "10:40", "10:50"]))
+    crews = []
+    for crew in timetable:
+        hr, mn = crew.split(':')
+        crew_time = int(hr) * 60 + int(mn)
+        crews.append(crew_time)
+    crews.sort()
+
+    crew_count = 0
+    bus_idx, crew_idx = 0, 0
+    last_crew_on_bus = -1  # 마지막 버스의 도착 시각
+    last_crew_count = 0  # 마지막 버스에 탄 인원
+    while bus_idx < len(buses) and crew_idx < len(crews):
+        bus_cur = buses[bus_idx]
+        if crews[crew_idx] <= bus_cur:  # 현재 사람 태울 수 있으면 태움
+            if bus_idx == len(buses) - 1:  # 마지막 버스 기록
+                last_crew_on_bus = crews[crew_idx]
+                last_crew_count += 1
+            crew_idx += 1
+            crew_count += 1
+        else:  # 현재 버스를 탈 인원이 없음 -> 다음 버스로!
+            crew_count = 0
+            bus_idx += 1
+
+        if crew_count >= m:  # 한 버스에 인원이 찬 경우, 다음 버스로!
+            crew_count = 0
+            bus_idx += 1
+
+    ans = 0
+    if last_crew_count < m:  # 만약 인원이 덜 찼으면 마지막 버스 도착 시각
+        ans = buses[-1]
+    else:  # 꽉 찼으면 마지막 인원 -1분
+        ans = last_crew_on_bus - 1
+
+    hr, mn = divmod(ans, 60)
+    hr = '0' + str(hr) if hr < 10 else str(hr)
+    mn = '0' + str(mn) if mn < 10 else str(mn)
+
+    return hr + ':' + mn
