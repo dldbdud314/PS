3edbc180d59257e25bbcc3121306241a5c0efdd5
dldbdud314 <dldbdud314@naver.com>
2022-12-07 12:46:43 +0900

    [update]: 네트워크

--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -1,25 +1,20 @@
-def dfs(x, graph, visited):
-    if visited[x] == True:
-        return
+def dfs(x, n, computers, visited):
     visited[x] = True
-    for val in graph[x]:
-        dfs(val, graph, visited)
 
-def make_graph(n, matrix):
-    graph = dict()
-    for i in range(n):
-        graph[i] = []
-        for j in range(n):
-            if i != j and matrix[i][j] == 1:
-                graph[i].append(j)
-    return graph
+    for y in range(n):
+        if y == x:
+            continue
+        if computers[x][y] == 1 and not visited[y]:
+            dfs(y, n, computers, visited)
+
 
 def solution(n, computers):
-    graph = make_graph(n, computers)
-    cnt = 0
     visited = [False] * n
-    for x in graph:
-        if visited[x] == False:
-            dfs(x, graph, visited)
-            cnt += 1
-    return cnt
+
+    count = 0
+    for x in range(n):
+        if not visited[x]:
+            dfs(x, n, computers, visited)
+            count += 1
+
+    return count
