1add048deafd24df23f70a092d1d2d4fe0384ac8
dldbdud314 <dldbdud314@naver.com>
2022-12-07 14:56:02 +0900

    [update]: 신고결과받기

--- programmers/신고결과받기.py
+++ programmers/신고결과받기.py
@@ -1,20 +1,23 @@
 def solution(id_list, report, k):
-    report_dict = {id : list() for id in id_list}
-    record = {id : 0 for id in id_list}
-    for x in report:
-        x_list = x.split()
-        if x_list[1] not in report_dict[x_list[0]]:
-            report_dict[x_list[0]].append(x_list[1]) 
-            record[x_list[1]] += 1
-    
-    report_list = []
-    for id in id_list:
-        report_list.append(report_dict[id])
-    
-    ans = []
-    for a in report_list:
-        num = 0
-        for aa in a:
-            if record[aa] >= k: num += 1
-        ans.append(num)
-    return ans
+    reported_id = {user_id: 0 for user_id in id_list}  # 누가 신고 몇 회 당했는지
+    report_stat = {user_id: list() for user_id in id_list}  # 누굴 신고했는지 저장
+
+    for x in set(report):
+        reporter, reported = x.split()
+        reported_id[reported] += 1
+        report_stat[reporter].append(reported)
+
+    suspended_ids = set()
+    for user_id, count in reported_id.items():
+        if count >= k:
+            suspended_ids.add(user_id)
+
+    res = [0] * len(id_list)
+    i = 0
+    for user_id, reported_list in report_stat.items():
+        for reported in reported_list:
+            if reported in suspended_ids:
+                res[i] += 1
+        i += 1
+
+    return res
