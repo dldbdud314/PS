c5e56a8d5203bd87374e77a5be6c8fe834377275
dldbdud314 <dldbdud314@naver.com>
2023-02-03 14:04:53 +0900

    [update]: N으로표현

--- programmers/N으로표현.py
+++ programmers/N으로표현.py
@@ -1,25 +1,36 @@
+"""
+완탐에 DP 얹기
+
+cnt = N을 사용한 횟수, DP[cnt] = set() -> N을 cnt개 활용해서 만들 수 있는 숫자 SET
+cnt 1 ~ 8까지 연산 하며, number와 일치하는 set이 나오면 바로 cnt RETURN
+
+한 라운드 당 SET: operate(dp[i], dp[cnt - i]) + N을 cnt개 이어 붙인 숫자
+"""
 from itertools import product
 
-def operate(set1, set2):
-    res = set()
-    for x, y in product(list(set1), list(set2)):
-        res.add(x + y)
-        res.add(x - y)
-        res.add(x * y)
-        if y != 0: res.add(x // y)
-    return res
+
+def operate(s1, s2):  # 연산 메소드
+    to_add = set()
+    for n1, n2 in product(s1, s2):
+        to_add.add(n1 + n2)
+        to_add.add(n1 - n2)
+        to_add.add(n1 * n2)
+        if n2 != 0: to_add.add(n1 // n2)
+    return to_add
+
 
 def solution(N, number):
-    if number == N: return 1 #edge case
-    dp = [0] * 10
-    dp[1] = {N}
-    for i in range(1, 9):
-        new_set = set()
-        new_set.add(int(str(N)*(i+1))) #NNN.. 형태
-        for j in range(1, i + 1):
-            new_set.update(operate(dp[j], dp[i+1-j]))
-        if number in new_set: return i + 1
-        dp[i + 1] = new_set
-    return -1
+    if N == number:  # 바로 RETURN
+        return 1
 
-print(solution(2, 11))
+    dp = [None] * 9
+    dp[1] = {N}  # N을 cnt개 사용해서 만들 수 있는 숫자 SET
+
+    for cnt in range(2, 9):  # 최대 8까지
+        dp[cnt] = {int(str(N) * cnt)}
+        for i in range(1, cnt):
+            dp[cnt].update(operate(dp[i], dp[cnt - i]))
+        if number in dp[cnt]:
+            return cnt
+
+    return -1
