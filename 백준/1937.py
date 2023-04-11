8f2721f9f183423b1158664508124119051d78cb
dldbdud314 <dldbdud314@naver.com>
2023-04-11 10:04:19 +0900

    [fix]: 1937

--- 백준/1937.py
+++ 백준/1937.py
@@ -1,6 +1,6 @@
 """
 1937. 욕심쟁이 판다
-** DFS + DP -> 시간초과, 풀이 수정 필요 .. !!
+** DFS + DP -> 수정 : 풀참
 """
 import sys
 sys.setrecursionlimit(10 ** 6)
@@ -8,25 +8,24 @@ sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 
-def dfs(cx, cy, cnt):
-    dp[cx][cy] = cnt
+def dfs(cx, cy):
+    if dp[cx][cy] == 0:
+        dp[cx][cy] = 1
 
-    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-        if 0 <= cx + dx < n and 0 <= cy + dy < n and MAP[cx + dx][cy + dy] < MAP[cx][cy] and dp[cx + dx][cy + dy] < cnt + 1:
-            dfs(cx + dx, cy + dy, cnt + 1)
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            if 0 <= cx + dx < n and 0 <= cy + dy < n and MAP[cx + dx][cy + dy] > MAP[cx][cy]:
+                dp[cx][cy] = max(dfs(cx + dx, cy + dy), dp[cx][cy])
+
+    return dp[cx][cy] + 1
 
 
 n = int(input())
 MAP = [list(map(int, input().split())) for _ in range(n)]
 
+res = 0
 dp = [[0] * n for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        if dp[i][j] == 0:
-            dfs(i, j, 1)
-
-res = 0
-for x in dp:
-    res = max(max(x), res)
+        res = max(res, dfs(i, j) - 1)
 
 print(res)
